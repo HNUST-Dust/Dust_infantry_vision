92e14d3af3065d0fc0bb605d4d7e1abe01d9e481
jy666317 <3466161916@qq.com>
2026-08-20 17:17:38 +0800

    限制日志文件磁盘占用

--- tools/logger.cpp
+++ tools/logger.cpp
@@ -1,13 +1,16 @@
 #include "logger.hpp"
 
-#include <fmt/chrono.h>
-#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
-#include <chrono>
 #include <filesystem>
-#include <string>
+
+namespace
+{
+constexpr std::size_t kMaxLogFileSize = 10 * 1024 * 1024;
+constexpr std::size_t kMaxRotatedLogFiles = 4;
+}  // namespace
 
 namespace tools
 {
@@ -16,8 +19,8 @@ std::shared_ptr<spdlog::logger> logger_ = nullptr;
 void set_logger()
 {
   std::filesystem::create_directories("logs");
-  auto file_name = fmt::format("logs/{:%Y-%m-%d_%H-%M-%S}.log", std::chrono::system_clock::now());
-  auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(file_name, true);
+  auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+    "logs/infantry.log", kMaxLogFileSize, kMaxRotatedLogFiles);
   file_sink->set_level(spdlog::level::debug);
 
   auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
